95abd742f7ea32e5d8f4398adb9e7d130696d2a6
Fan2k28 <rochinellastrad@gmail.com>
2026-09-09 11:32:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['237686241566'], // Add your number without + or spaces
+    ownerNumber: ['237696373320'], // Add your number without + or spaces
     ownerName: ['Giluce Bot', 'Admin'],
     
     // Bot Configuration
